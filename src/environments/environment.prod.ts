cfbff670955ec40c4c8fa7a84ea4fe73e39dbe42
Noah-Josua Hartmann <noah.josua@posteo.de>
2024-02-10 13:54:09 +0100

    change environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://localhost:8080' // 'https://webentwicklungbasicsbackend-production.up.railway.app'
+    baseUrl: 'http://localhost:8080' 
 }
